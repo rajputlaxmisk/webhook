e30fecbff4bfd5eaa60d3981e5a290ef1638d61c
rajputlaxmisk <rajputlaxmisk@gmail.com>
2020-08-12 23:54:40 +0530

    wh

--- src/main/java/com/project1/controller.java
+++ src/main/java/com/project1/controller.java
@@ -1,5 +1,9 @@
 package com.project1;
 
+import java.util.Map;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,5 +15,9 @@ public class controller {
 	{
 		return "Hello";
 	}
+	@PostMapping("/webhook")
+    public void getMyJson(@RequestBody Map<String, Object> json) {
+    System.out.println("WebHook collected JSON: " + json);
+    }
 
 }
